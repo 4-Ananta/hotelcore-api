95ae249e89478778d6a42934ce45c84db6ddc336
4-Ananta <anantarisky423@gmail.com>
2026-07-12 23:28:25 +0700

    Set up Spring Boot backend with MySQL and JPA

---
+++ backend/hotelcore-backend/src/main/java/com/hotelcore/HotelcoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hotelcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelcoreBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelcoreBackendApplication.class, args);
+    }
+
+}
